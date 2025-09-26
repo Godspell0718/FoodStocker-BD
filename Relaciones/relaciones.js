341488c703c28fed621152e836709cd5516a18f2
Kimarutsu <juansebastiancamposcampoos@gmail.com>
2025-09-26 08:41:30 -0500

    na mas le puse a que el carrusel cambiara solo y mejore el js

--- Relaciones/relaciones.js
+++ Relaciones/relaciones.js
@@ -1,93 +1,72 @@
-let imagen1 = '../img/DestinoSalidas.jpeg';
+let imagenes = [
+    '../img/DestinoSalidas.jpeg',
+    '../img/EntradasAlmacen.jpeg',
+    '../img/InsumoEntradas.jpeg',
+    '../img/UsuarioSalidas.jpeg'
+];
 
-if (imagen1 == '../img/DestinoSalidas.jpeg') {
-    document.getElementById('botondestinoSalidas').style.border = 'solid 5px black';
+let textos = [
+    'Relacion destino salidas',
+    'Relacion entradas almacen',
+    'Relacion insumo entradas',
+    'Relacion usuario salidas'
+];
+
+let index = 0; 
+let intervalo; 
+
+document.getElementById('imagen').src = imagenes[index];
+document.getElementById('animal').innerText = textos[index];
+document.getElementById('botondestinoSalidas').style.border = 'solid 5px black';
+
+function iniciarCarrusel() {
+    intervalo = setInterval(() => {
+        siguiente();
+    }, 5000);
 }
+
+function reiniciarCarrusel() {
+    clearInterval(intervalo);
+    iniciarCarrusel();
+}
+
+function mostrarImagen() {
+   
+    document.getElementById('botondestinoSalidas').style.border = 'none';
+    document.getElementById('botonentradasAlmacen').style.border = 'none';
+    document.getElementById('botoninsumoEntradas').style.border = 'none';
+    document.getElementById('botonusuarioSalidas').style.border = 'none';
+
+
+    document.getElementById('imagen').src = imagenes[index];
+    document.getElementById('animal').innerText = textos[index];
+
+
+    if (index === 0) document.getElementById('botondestinoSalidas').style.border = 'solid 5px black';
+    if (index === 1) document.getElementById('botonentradasAlmacen').style.border = 'solid 5px black';
+    if (index === 2) document.getElementById('botoninsumoEntradas').style.border = 'solid 5px black';
+    if (index === 3) document.getElementById('botonusuarioSalidas').style.border = 'solid 5px black';
+}
+
 function cambiarimg(numero) {
-
-    document.getElementById('botondestinoSalidas').style.border = 'none';
-    document.getElementById('botonentradasAlmacen').style.border = 'none';
-    document.getElementById('botoninsumoEntradas').style.border = 'none';
-    document.getElementById('botonusuarioSalidas').style.border = 'none';
-
-    if (numero == 1) {
-        imagen1 = '../img/DestinoSalidas.jpeg';
-        document.getElementById('botondestinoSalidas').style.border = 'solid 5px black'
-        document.getElementById('animal').innerText = 'Relacion destino salidas';
-
-    } else if (numero == 2) {
-        imagen1 = '../img/EntradasAlmacen.jpeg';
-        document.getElementById('botonentradasAlmacen').style.border = 'solid 5px black'
-        document.getElementById('animal').innerText = 'Relacion entradas almacen';
-
-    } else if (numero == 3) {
-        imagen1 = '../img/InsumoEntradas.jpeg';
-        document.getElementById('botoninsumoEntradas').style.border = 'solid 5px black'
-        document.getElementById('animal').innerText = 'Relacion insumo entradas';
-
-    } else if (numero == 4) {
-        imagen1 = '../img/UsuarioSalidas.jpeg';
-        document.getElementById('botonusuarioSalidas').style.border = 'solid 5px black'
-        document.getElementById('animal').innerText = 'Relacion usuario salidas';
-    }
-    
-    document.getElementById('imagen').src = imagen1;
+    index = numero - 1; 
+    mostrarImagen();
+    reiniciarCarrusel();
 }
+
 function botones(direccion) {
-
-    document.getElementById('botondestinoSalidas').style.border = 'none';
-    document.getElementById('botonentradasAlmacen').style.border = 'none';
-    document.getElementById('botoninsumoEntradas').style.border = 'none';
-    document.getElementById('botonusuarioSalidas').style.border = 'none';
-
-    if (direccion == 'L') {
-
-        if (imagen1 === '../img/DestinoSalidas.jpeg') {
-            imagen1 = '../img/EntradasAlmacen.jpeg';
-            document.getElementById('botonusuarioSalidas').style.border = 'solid 5px black';
-            document.getElementById('animal').innerText = 'Relacion Usuario salidas';
-
-        } else if (imagen1 === '../img/EntradasAlmacen.jpeg') {
-            imagen1 = '../img/InsumoEntradas.jpeg';
-            document.getElementById('botoninsumoEntradas').style.border = 'solid 5px black';
-            document.getElementById('animal').innerText = 'Relacion insumo entradas';
-
-        } else if (imagen1 === '../img/InsumoEntradas.jpeg') {
-            imagen1 = '../img/UsuarioSalidas.jpeg';
-            document.getElementById('botonentradasAlmacen').style.border = 'solid 5px black';
-            document.getElementById('animal').innerText = 'Relacion entradas almacen';
-
-        } else if (imagen1 === '../img/UsuarioSalidas.jpeg') {
-            imagen1 = '../img/DestinoSalidas.jpeg';
-            document.getElementById('botondestinoSalidas').style.border = 'solid 5px black';
-            document.getElementById('animal').innerText = 'Relacion destino salidas';
-
-        }
-
-    } else if (direccion == 'R') {
-
-        if (imagen1 === '../img/DestinoSalidas.jpeg') {
-            imagen1 = '../img/UsuarioSalidas.jpeg';
-            document.getElementById('botonentradasAlmacen').style.border = 'solid 5px black';
-            document.getElementById('animal').innerText = 'Relacion entradas almacen';
-
-        } else if (imagen1 === '../img/UsuarioSalidas.jpeg') {
-            imagen1 = '../img/InsumoEntradas.jpeg';
-            document.getElementById('botoninsumoEntradas').style.border = 'solid 5px black';
-            document.getElementById('animal').innerText = 'Relacion insumo entradas';
-
-        } else if (imagen1 === '../img/InsumoEntradas.jpeg') {
-            imagen1 = '../img/EntradasAlmacen.jpeg';
-            document.getElementById('botonusuarioSalidas').style.border = 'solid 5px black';
-            document.getElementById('animal').innerText = 'Relacion Usuario salidas';
-
-        } else if (imagen1 === '../img/EntradasAlmacen.jpeg') {
-            imagen1 = '../img/DestinoSalidas.jpeg';
-            document.getElementById('botondestinoSalidas').style.border = 'solid 5px black';
-            document.getElementById('animal').innerText = 'Relacion destino salidas';
-
-        }
+    if (direccion === 'L') {
+        index = (index - 1 + imagenes.length) % imagenes.length;
+    } else if (direccion === 'R') {
+        index = (index + 1) % imagenes.length;
     }
-    document.getElementById('imagen').src = imagen1;
+    mostrarImagen();
+    reiniciarCarrusel();
 }
 
+function siguiente() {
+    index = (index + 1) % imagenes.length;
+    mostrarImagen();
+}
+
+window.onload = iniciarCarrusel;
